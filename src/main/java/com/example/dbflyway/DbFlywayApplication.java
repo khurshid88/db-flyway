43e02c14d3f0e857cbc6db50d200b3c6f762775c
xurshidbek <shamsun.com@gmail.com>
2023-09-20 09:12:41 +0500

    Initial commit

---
+++ src/main/java/com/example/dbflyway/DbFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbFlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbFlywayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dbflyway/DbFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbFlywayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
